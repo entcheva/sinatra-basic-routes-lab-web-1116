64b84149227ba196742fbb881de4a26e17a7ae74
entcheva <christina.entcheva@gmail.com>
2016-12-15 12:04:06 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+  get '/name' do
+    "My name is Christina"
+  end
+
+  get '/hometown' do
+    "My hometown is Sofia, Bulgaria"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Soft Moon, 'Breathe The Fire'"
+  end
+
+
+
+
+
 end
